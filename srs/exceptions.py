6d0fadcdba64f73f6caa4b6d876f4abecad4edbd
priyanshuc0007 <priyanshuc111@gmail.com>
2023-09-23 11:13:17 +0530

    model trainer

--- srs/exceptions.py
+++ srs/exceptions.py
@@ -15,6 +15,4 @@ class customexception(Exception):
         super().__init__(error_message)
         self.error_message = error_message_detail(error_message, error_detail)
     
-    def __str__(self):
-        return self.error_message
-
+    
