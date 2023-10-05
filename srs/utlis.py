9ec63c49310fd543ac366d24eadefa8fd71ff0bb
priyanshuc0007 <122784189+priyanshuc0007@users.noreply.github.com>
2023-10-05 13:27:47 +0530

    Revert "Revert "Git add  ""

--- srs/utlis.py
+++ srs/utlis.py
@@ -5,15 +5,11 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
 from srs.exceptions import customexception  # Ensure this import is correct
+from sklearn.model_selection import GridSearchCV
+import pickle
 
 def save_object(file_path, obj):
-    """
-    Save a Python object to a file using dill serialization.
     
-    Parameters:
-        file_path (str): The path to the file where the object will be saved.
-        obj (object): The Python object to be saved.
-    """
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
@@ -24,32 +20,40 @@ def save_object(file_path, obj):
     except Exception as e:
         raise customexception(e, sys)
 
-def evaluate_models(x_train, y_train, x_test, y_test, models):
-    """
-    Evaluate machine learning models on training and testing data.
-    
-    Parameters:
-        x_train (array-like): Training features.
-        y_train (array-like): Training target values.
-        x_test (array-like): Testing features.
-        y_test (array-like): Testing target values.
-        models (dict): A dictionary of model names and corresponding model objects.
-        
-    Returns:
-        dict: A dictionary of model names and their R-squared scores on the testing data.
-    """
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
-        report = {}  # Initialize the report dictionary
-        
-        for model_name, model in models.items():
-            model.fit(x_train, y_train)  # Train model
-            y_train_pred = model.predict(x_train)
-            y_test_pred = model.predict(x_test)
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
             train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
-            report[model_name] = test_model_score
-        
-        return report  # Return the report dictionary after evaluating all models
-        
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise customexception(e, sys)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise customexception(e, sys)
